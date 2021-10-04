d062e4401ec0699780d29ba7f6e7a955c52f1058
JOBLIN-K. JAMES <31209860+jobzz-kj@users.noreply.github.com>
2021-10-04 19:24:58 +0530

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,18 @@
+import turtle
+
+#This is a square
+jobzz_turtle=turtle.Turtle()
+
+def square():
+  
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
+  jobzz_turtle.right(90)
+  jobzz_turtle.forward(100)
+
+square()
+jobzz_turtle.forward(100)
+square()
